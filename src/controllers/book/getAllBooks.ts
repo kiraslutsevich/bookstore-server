c2ee653aae827023f65b92cd0cab7c3841dbe3b0
kira <kira.bat21@gmail.com>
2022-08-17 18:31:08 +0300

    ---

--- src/controllers/book/getAllBooks.ts
+++ src/controllers/book/getAllBooks.ts
@@ -1,5 +1,6 @@
 import { RequestHandler } from 'express';
 import { StatusCodes } from 'http-status-codes';
+import { Between, FindManyOptions, ILike } from 'typeorm';
 import { Book } from '../../db/entity/Book';
 import type { EmptyObject } from '../../utils/types';
 import createCustomError from '../../utils/createCustomError';
@@ -7,6 +8,18 @@ import db from '../../db';
 
 type ReqParams = {
   id: string;
+
+}
+
+type ReqQuery = {
+  column?: string;
+  order?: 'ASC' | 'DESC';
+  perPage?: number;
+  page?: number;
+  minPrice?: number;
+  maxPrice?: number;
+  search?: string;
+  genresId?: number[];
 }
 
 type ResBody = Book[];
@@ -15,18 +28,56 @@ type ControllerType = RequestHandler<
 ReqParams,
 ResBody,
 EmptyObject,
-EmptyObject
+ReqQuery
 >
 
 const getAllBooks: ControllerType = async (req, res, next) => {
+  console.log(req.query.genresId)
   try {
-    const books = await db.book.find();
+    const order = {
+      [req.query.column]: req.query.order,
+    };
+    const take = req.query.perPage || 12;
+    const page = +req.query.page || 1;
+    const skip = take ? (page - 1) * take : null;
+    const price = Between(req.query.minPrice || 0, req.query.maxPrice || 10000);
+    let where: FindManyOptions<Book>['where'];
+
+    if (req.query.search) {
+      const searchQuery = ILike(`%${req.query.search}%`);
+      where = [
+        { name: searchQuery, price },
+        { author: searchQuery, price },
+        // { desription: searchQuery, price },
+      ];
+    } else if (req.query.genresId) {
+      const arr = req.query.genresId.map((genre) => {
+        return { id: genre };
+      });
+      where = {
+        genres: arr,
+        price,
+      };
+    } else {
+      where = { price };
+    }
+
+    const [books, totalCount] = await db.book.findAndCount({
+      relations: {
+        genres: true,
+      },
+      where,
+      order,
+      skip,
+      take,
+    });
+
     if (!books) {
       throw createCustomError(StatusCodes.NOT_FOUND, 'books not found');
     }
     return res.status(StatusCodes.OK).json(books);
   } catch (err) {
-    console.log(err)
+    console.log(err);
     next(err);
   }
 };
